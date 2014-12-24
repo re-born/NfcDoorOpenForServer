bd2422765196cfa20365cb95fd640df449ac3023
daichi <daipanchi@gmail.com>
2014-12-24 22:50:23 +0900

    Add administration page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,15 @@
 class UserController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   mount API => '/'
   mount GrapeSwaggerRails::Engine => '/apidoc'
+
+  root 'user#index'
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
